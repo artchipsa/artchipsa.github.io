7251fcdd3de1e9e8fe15d37944f08c1c2d41f460
artchipsa <art@chipsa.ru>
2016-08-23 16:42:28 +0700

    Add index

--- altan-blog/builds/dev/js/main.js
+++ altan-blog/builds/dev/js/main.js
@@ -31,6 +31,53 @@ $(document).ready(function(){
 		});
 	}
 
+
+	//header-search
+
+	jQuery.fn.animateAuto = function(prop, speed, callback){
+	    var elem, height, width;
+	    return this.each(function(i, el){
+	        el = jQuery(el), elem = el.clone().css({"height":"auto","width":"auto"}).appendTo("body");
+	        height = elem.css("height"),
+	        width = elem.css("width"),
+	        elem.remove();
+	        
+	        if(prop === "height")
+	            el.animate({"height":height}, speed, callback);
+	        else if(prop === "width")
+	            el.animate({"width":width}, speed, callback);  
+	        else if(prop === "both")
+	            el.animate({"width":width,"height":height}, speed, callback);
+	    });  
+	}
+
+	$('.search').click(function(){
+		var btn = $(this);
+		if (btn.hasClass('open')){
+			btn.find('span').removeClass('glyphicon-remove').addClass('glyphicon-search');
+			btn.parent().find('input[type="text"]').fadeOut(300);
+			setTimeout(function(){
+				btn.parent().animateAuto('width', 350, function(){
+					if($(window).width() > 768){
+						$('.btn.header').fadeIn(300);
+					} else{
+						$('.m-subscribe').fadeIn(300);
+					}
+				});
+			}, 300);
+			btn.removeClass('open');
+		} else {
+			$('.btn.header, .m-subscribe').fadeOut(300);
+			setTimeout(function(){
+				btn.parent().animate({width: "100%"}, 350, function(){
+					btn.find('span').removeClass('glyphicon-search').addClass('glyphicon-remove');
+					btn.parent().find('input[type="text"]').fadeIn(300);
+				});
+			}, 300);
+			btn.addClass('open');
+		}
+	});
+
 	var header_width = $('header').height();
 	$('#header_placeholder').height(header_width);
 	$(window).scroll(function(){
@@ -44,11 +91,9 @@ $(document).ready(function(){
 	});
 
 	$(document).on('click', '.top', function(){
-		$('body, html').stop().animate({scrollTop: car_offset.top}, '3550');
+		$('body, html').stop().animate({scrollTop: car_offset.top - $('header').height()}, '3550');
 		return false;
 	});	
-
-
 
 	//like-func
 
@@ -103,7 +148,142 @@ $(document).ready(function(){
 	  	return false;
 	});
 
+	//map
+	var myMap;
+	ymaps.ready(function(){
+		myMap = new ymaps.Map('map', {
+			zoom: 11,
+			center: [48.46897578085214,135.1129739999999]
+		});
+		myMap.controls.add('zoomControl', { left: 5, top: 50 });
+
+		//Centers
+		var Khabarovsk = [48.46897578085214,135.1129739999999];
+		var Komsomolsk = [50.57667290137674,137.01914949999983];
+		var Beerobidjan = [48.78065959694758,132.9329115];
+
+		Habar1 = new ymaps.Placemark([48.492608573874726,135.10099549999993], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/marker.png',
+            iconImageSize: [60, 80],
+            iconImageOffset: [-30, -80]
+        });	
+        Habar2 = new ymaps.Placemark([48.53565807385006,135.0685119999999], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/marker.png',
+            iconImageSize: [60, 80],
+            iconImageOffset: [-30, -80]
+        });	
+        Habar3 = new ymaps.Placemark([48.39910707393176,135.09345849999985], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/marker.png',
+            iconImageSize: [60, 80],
+            iconImageOffset: [-30, -80]
+        });
+        Komsomol = new ymaps.Placemark([50.53838907295097,137.03249899999994], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/marker.png',
+            iconImageSize: [60, 80],
+            iconImageOffset: [-30, -80]
+        });
+        Beer = new ymaps.Placemark([48.76614207379159,132.94654749999998], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/marker.png',
+            iconImageSize: [60, 80],
+            iconImageOffset: [-30, -80]
+        });
+        myMap.geoObjects.add(Habar1);
+		myMap.geoObjects.add(Habar2);
+		myMap.geoObjects.add(Habar3);
+        $(document).on('click', '.city', function(){
+	  		var text = $(this).text();
+	  		$(this).parents('.dropdown').find('.text').text(text);
+	  		$(this).parents('.dropdown').find('.dropdown-menu').first().stop(true, true).slideUp('fast');
+	  		var city = $(this).data('city');
+	  		switch(city){
+	  			case 'Khabarovsk':
+	  				myMap.setZoom(11);
+	  				myMap.setCenter(Khabarovsk);
+	  				myMap.geoObjects.add(Habar1);
+	  				myMap.geoObjects.add(Habar2);
+	  				myMap.geoObjects.add(Habar3);
+	  				$('.shops').fadeOut(300);
+	  				$('.shops[data-city="Khabarovsk"]').fadeIn(300);
+	  				break;
+	  			case 'Komsomolsk':
+	  				myMap.setZoom(11);
+	  				myMap.setCenter(Komsomolsk);
+	  				myMap.geoObjects.add(Komsomol);
+	  				$('.shops').fadeOut(300);
+	  				$('.shops[data-city="Komsomolsk"]').fadeIn(300);
+	  				break;
+	  			case 'Beerobidjan':
+	  				myMap.setZoom(11);
+	  				myMap.setCenter(Beerobidjan);
+	  				myMap.geoObjects.add(Beer);
+	  				$('.shops').fadeOut(300);
+	  				$('.shops[data-city="Beerobidjan"]').fadeIn(300);
+	  				break;
+	  		}
+	  		return false;
+	  	});
+
+	  	$(document).on('click', '.marker', function(){
+	  		var id = $(this).data('id');
+	  		switch(id){
+	  			case 'habar1':
+	  				myMap.setZoom(16, {
+	  					callback: function(){
+	  						myMap.panTo([Habar1.geometry._yc[0], Habar1.geometry._yc[1]], {delay: 0, checkZoomRange: true});
+	  					}
+	  				});
+	  				break;
+	  			case 'habar2': 
+	  				myMap.setZoom(16, {
+	  					callback: function(){
+	  						myMap.panTo([Habar2.geometry._yc[0], Habar2.geometry._yc[1]], {delay: 0, checkZoomRange: true});
+	  					}
+	  				});
+	  				break;
+	  			case 'habar3': 
+	  				myMap.setZoom(16, {
+	  					callback: function(){
+	  						myMap.panTo([Habar3.geometry._yc[0], Habar3.geometry._yc[1]], {delay: 0, checkZoomRange: true});
+	  					}
+	  				});
+	  				break;
+	  			case 'Komsomol': 
+	  				myMap.setZoom(16, {
+	  					callback: function(){
+	  						myMap.panTo([Komsomol.geometry._yc[0], Komsomol.geometry._yc[1]], {delay: 0, checkZoomRange: true});
+	  					}
+	  				});
+	  				break;
+	  			case 'Beer': 
+	  				myMap.setZoom(16, {
+	  					callback: function(){
+	  						myMap.panTo([Beer.geometry._yc[0], Beer.geometry._yc[1]], {delay: 0, checkZoomRange: true});
+	  					}
+	  				});
+	  				break;
+
+	  		}
+	  		return false;
+	  	});
+	});
+
+	$('.dropdown').on('show.bs.dropdown', function(e){
+	    $(this).find('.dropdown-menu').first().stop(true, true).slideDown('fast');
+	  });
+
+  	$('.dropdown').on('hide.bs.dropdown', function(e){
+	    $(this).find('.dropdown-menu').first().stop(true, true).slideUp('fast');
+  	});
+
+
 });
+
+
 
 
 function specialImgPosition(){
