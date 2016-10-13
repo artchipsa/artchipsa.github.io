f2529b5ec4f658c3ec511e5cbe3bea055170b784
artchipsa <art@chipsa.ru>
2016-10-13 09:51:17 +0700

    final vidos?

--- AVAKS/js/video.js
+++ AVAKS/js/video.js
@@ -131,22 +131,24 @@ $(document).ready(function(){
   }
 
   var vid = document.getElementById('v0');
-  var windowheight = $(window).height();
+  // var windowheight = $(window).height();
+  var windowheight = $(window).height()-20;
 
-  var scrollpos = window.pageYOffset/350;
+
+  var scrollpos = window.pageYOffset/400;
   var targetscrollpos = scrollpos;
   var accel = 0;
 
+
   // ---- Values you can tweak: ----
-  var accelamount = 0; //How fast the video will try to catch up with the target position. 1 = instantaneous, 0 = do nothing.
-  var bounceamount = 0.9; //value from 0 to 1 for how much backlash back and forth you want in the easing. 0 = no bounce whatsoever, 1 = lots and lots of bounce
+  var accelamount = 0.1; //How fast the video will try to catch up with the target position. 1 = instantaneous, 0 = do nothing.
 
   // pause video on load
   vid.pause();
    
   window.onscroll = function(){
       //Set the video position that we want to end up at:
-      targetscrollpos = window.pageYOffset/350;
+      targetscrollpos = window.pageYOffset/400;
       updateProgressBar();
   };
   //Progress bar
@@ -162,16 +164,11 @@ $(document).ready(function(){
 
   setInterval(function(){  
           
+        
       //Accelerate towards the target:
-      accel += (targetscrollpos - scrollpos)*accelamount;
-      
-      //clamp the acceleration so that it doesnt go too fast
-      if (accel > 1) accel = 1;
-      if (accel < -1) accel = -1;
-
-      //move the video scroll position according to the acceleration and how much bouncing you selected:
-      scrollpos = (scrollpos + accel) * (bounceamount) + (targetscrollpos * (1-bounceamount));
-
+      scrollpos += (targetscrollpos - scrollpos)*accelamount;
+  
+  
       //update video playback
       vid.currentTime = scrollpos;
       vid.pause();
